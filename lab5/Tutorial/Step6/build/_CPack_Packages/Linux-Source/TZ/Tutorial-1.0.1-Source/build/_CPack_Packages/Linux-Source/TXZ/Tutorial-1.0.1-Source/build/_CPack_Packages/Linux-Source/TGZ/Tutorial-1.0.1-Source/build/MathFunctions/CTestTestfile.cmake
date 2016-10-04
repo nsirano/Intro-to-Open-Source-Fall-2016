d2812e661dab3a450e6ad39c5ac0f3144efd745f
nsirano <nsirano@gmail.com>
2016-10-04 13:44:25 -0400

    Updated Step6 with build directories

---
+++ lab5/Tutorial/Step6/build/_CPack_Packages/Linux-Source/TZ/Tutorial-1.0.1-Source/build/_CPack_Packages/Linux-Source/TXZ/Tutorial-1.0.1-Source/build/_CPack_Packages/Linux-Source/TGZ/Tutorial-1.0.1-Source/build/MathFunctions/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nick/School/Open_Source/Labs/My_Labs--CSCI2961_Fall_2016/lab5/Tutorial/Step6/MathFunctions
+# Build directory: /home/nick/School/Open_Source/Labs/My_Labs--CSCI2961_Fall_2016/lab5/Tutorial/Step6/build/MathFunctions
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
